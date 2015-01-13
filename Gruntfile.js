12653e48661e0e157b31bc53aae2429b0f5a2dcf
Adam Detrick <akdetrick@gmail.com>
2015-01-12 20:28:06 -0500

    task for ghpages

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-hologram');
+	grunt.loadNpmTasks('grunt-shell');
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -18,8 +19,17 @@ module.exports = function(grunt) {
 					config: 'docs/config.yml'
 				}
 			}
+		},
+		shell: {
+			options: {
+				stderr: false
+			},
+			target: {
+				command: './ghpages.sh'
+			}
 		}
 	});
 
 	grunt.registerTask('default', ['sass', 'hologram']);
+	grunt.registerTask('ghpages', ['sass', 'hologram', 'shell']);
 };
