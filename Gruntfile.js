b215e71fe5ee183fc1de0d89c0cbcb17952fcdfb
Adam Detrick <akdetrick@gmail.com>
2015-01-15 11:07:13 -0500

    clean before build

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,31 +3,36 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-hologram');
 	grunt.loadNpmTasks('grunt-gh-pages');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	var DIR_DOC_SRC = 'docs/',
+		DIR_BUILD = DIR_DOC_SRC + 'build/';
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		'sass': {
 			dist: {
 				files: {
-				"docs/templates/css/sassquatch.css": "sass/sassquatch.scss"
+					"docs/templates/css/sassquatch.css": "sass/sassquatch.scss"
 				}
 			}
 		},
 		'hologram': {
 			generate: {
 				options: {
-					config: 'docs/config.yml'
+					config: DIR_DOC_SRC+'/config.yml'
 				}
 			}
 		},
 		'gh-pages': {
 			options: {
-				base: 'docs/build/'
+				base: DIR_BUILD
 			},
 			src: ['**']
-		}
+		},
+		'clean': [DIR_BUILD]
 	});
 
-	grunt.registerTask('default', ['sass', 'hologram']);
-	grunt.registerTask('ghpages', ['sass', 'hologram', 'gh-pages']);
+	grunt.registerTask('default', ['clean', 'sass', 'hologram']);
+	grunt.registerTask('ghpages', ['default', 'gh-pages']);
 };
