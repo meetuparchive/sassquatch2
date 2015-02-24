6d4f289622e37fba58dec9ecb8899368739dc561
Adam Detrick <akdetrick@gmail.com>
2015-02-24 16:11:39 -0500

    add preprocess

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-hologram');
 	grunt.loadNpmTasks('grunt-gh-pages');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-preprocess');
 
 	var DIR_DOC_SRC = 'docs/',
 		DIR_BUILD = DIR_DOC_SRC + 'build/';
@@ -30,9 +31,22 @@ module.exports = function(grunt) {
 			},
 			src: ['**']
 		},
-		'clean': [DIR_BUILD]
+		'clean': [DIR_BUILD],
+		'preprocess': {
+			inline: {
+				src: ['docs/templates/_header.html'],
+				options: {
+					inline: true,
+					context: {
+						DEBUG: false,
+						'RELEASE_TAG': 'hotdogs, hotdogs, hotdogs'
+					}
+				}
+			}
+		}
 	});
 
+	grunt.registerTask('version', ['clean', 'preprocess', 'hologram']);
 	grunt.registerTask('default', ['clean', 'sass', 'hologram']);
 	grunt.registerTask('ghpages', ['sass', 'hologram', 'gh-pages']);
 };
