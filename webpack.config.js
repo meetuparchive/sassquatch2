2fe6618ae97e3e1c62fa0b1f485552ae94e4da71
Adam Detrick <akdetrick@gmail.com>
2016-07-14 13:52:41 -0400

    use sass as entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 var ExtractPlugin = require('extract-text-webpack-plugin');
 
-var PATH_ENTRY = path.resolve(__dirname, 'webpack.entry.js');
+var PATH_ENTRY = path.resolve(__dirname, 'sass', 'sassquatch.scss');
 var PATH_BUNDLE_DEST = path.resolve(__dirname, 'docs', 'templates', 'bundle');
 
 module.exports = {
@@ -15,15 +15,15 @@ module.exports = {
 		]
 	},
 
-	debug: true,
-
 	plugins: [
 		new ExtractPlugin("sassquatch.css")
 	],
 
 	// webpack requires a js entry point
 	// and bundle location
-	entry: "./webpack.entry.js",
+	entry: {
+		sassquatch: [PATH_ENTRY]
+	},
 	output: {
 		path: PATH_BUNDLE_DEST,
 		filename: 'bundle.js'
